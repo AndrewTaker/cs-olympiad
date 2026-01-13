672781620b34618e98f465d9308e9ea4936f3ff7
Vladislav Shtirmer <107852190+AndrewTaker@users.noreply.github.com>
2026-01-13 15:12:56 +0300

    Simplify pip install command in start.ps1
    Removed redundant pip install command for system-wide installation.

--- start.ps1
+++ start.ps1
@@ -2,7 +2,7 @@ powershell -ExecutionPolicy ByPass -c "irm https://astral.sh/uv/install.ps1 | ie
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
 uv python install 3.14
 
-uv pip install --system `
+uv pip install `
     numpy `
     pandas `
     matplotlib `
